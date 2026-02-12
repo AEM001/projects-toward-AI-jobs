917aa1fb3b2d0213e63844e6aeda46e6834cfe08
AEM001 <2695210474@qq.com>
2026-02-12 23:48:21 +0800

    hovertime app and JWT

---
+++ HoverTime/HoverTime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HoverTime
+//
+//  Created by Albert on 2/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HoverTime/HoverTime/HoverTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoverTimeApp.swift
+//  HoverTime
+//
+//  Created by Albert on 2/12/26.
+//
+
+import SwiftUI
+
+@main
+struct HoverTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
