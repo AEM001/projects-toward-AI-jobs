866e231f564f9a670a78f73a4e06646a1f4611bb
AEM001 <2695210474@qq.com>
2026-02-27 00:16:59 +0800

    first call

--- learn/agent/llm/call.py
+++ learn/agent/llm/call.py
@@ -1,1 +1,15 @@
-https://api.apicore.ai/v1/chat/completions
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+client=OpenAI(base_url=os.getenv("BASE_URL"),api_key=os.getenv("API_KEY"))
+
+response=client.chat.completions.create(
+    model="gemini-2.0-flash",
+    messages=[
+        {"role":"user","content":"tell me in detail how to use python to call OpenAI api"}
+    ]
+)
+
+print(response.choices[0].message.content)
